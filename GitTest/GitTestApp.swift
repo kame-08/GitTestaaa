fa722c29c1fe9970b6554da0b524425cc2205c33
ryo <21cm0108@jec.ac.jp>
2022-11-25 11:31:51 +0900

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Ryo on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
